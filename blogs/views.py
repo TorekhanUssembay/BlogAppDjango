3828e10f8d43dc59dcc7fbcfde63a18e7e9c050f
Your Name <you@example.com>
2026-01-27 21:44:34 +0500

    template inheritance + context processor

--- blogs/views.py
+++ blogs/views.py
@@ -1,2 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import Blog, Category
 
+def posts_by_category(request, category_id):
+    #Fetching the posts that belongs to category with the category_ID
+    posts = Blog.objects.filter(status='Published', category=category_id)
+    
+    #try:
+    #    category = Category.objects.get(pk=category_id)
+    #except:
+    #    return redirect('home')
+    
+    category = get_object_or_404(Category, pk=category_id)
+    context = {
+        'posts': posts,
+        'category': category,
+    }
+    return render(request, 'posts_by_category.html', context)
